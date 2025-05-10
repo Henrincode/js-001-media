82285f6fcbac7834a2a4ace7d824a8ca8f494c90
Henrique inCode <henrincode@gmail.com>
2025-05-09 22:29:14 -0300

    desafio 1 ok

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4
 let media = (n1 + n2 + n3 + n4) / 4
 
 console.log("A média é: " + media.toFixed(2))
+
+if(media >= 5){
+    console.log("Parabéns, você passou")
+} else {
+    console.log("Que pena, volte na próxima")
+}
