cbdd60bcca7edfeb2a920acfffc78c56c414fd3d
Henrincode <spellctro@gmail.com>
2025-05-10 00:27:10 -0300

    organizando desafios no arquivo app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4
-let media = (n1 + n2 + n3 + n4) / 4
+// prettier-ignore
+let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4;
+let media = (n1 + n2 + n3 + n4) / 4;
 
-console.log("A média é: " + media.toFixed(2))
+console.log("A média é: " + media.toFixed(2));
 
-if(media >= 5){
-    console.log("Parabéns, você passou")
+// 1) Dependendo da nota, mostrar se a aluna ou aluno foi aprovada(o) ou não
+if (media >= 5) {
+  console.log("Parabéns, você passou");
 } else {
-    console.log("Que pena, volte na próxima")
+  console.log("Que pena, volte na próxima");
 }
+
+// 2) Alterar o fundo da tela da maneira que achar mais legal
+
+// 3) Imprimir na própria página o resultado, ao invés do console
+
+// 4) Criar um conversor de temperaturas entre farenheit e celcius
+
+// 5) Colocar a conta inteira da média em apenas uma linha
