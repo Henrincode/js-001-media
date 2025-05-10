3233bdbf836dc70a841a6671e1c755198d1d89ed
Henrincode <spellctro@gmail.com>
2025-05-10 00:42:32 -0300

    resolvido desafio 3) imprimir na página

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ document.body.style.backgroundColor = "#04394e";
 
 // 3) Imprimir na própria página o resultado, ao invés do console
 
+// cria a mensagem com a média formatada
+let mensagemMedia = `A média é: ${media.toFixed(2)}.`;
+
+// define a mensagem de aprovação ou reprovação com base na média
+let mensagemAprovadoReprovado;
+
+if (media >= 5) {
+  mensagemAprovadoReprovado = "Parabéns, você passou";
+} else {
+  mensagemAprovadoReprovado = "Que pena, volte na próxima";
+}
+
+// obtém a referência ao elemento HTML onde o resultado será exibido
+let conteudo = document.getElementById("conteudo");
+
+// insere o conteúdo HTML gerado dentro da div com id "conteudo"
+conteudo.innerHTML = `
+    <p>${mensagemMedia}<br>
+    ${mensagemAprovadoReprovado}</p>`;
+
 // 4) Criar um conversor de temperaturas entre farenheit e celcius
 
 // 5) Colocar a conta inteira da média em apenas uma linha
